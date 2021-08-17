2955c33370e053e809c5ef6ef0004d867a4a7135
jongwanra <najongsi@gmail.com>
2021-08-17 20:05:45 +0900

    add dynamic programming problem

--- python_baekjoon/dynamic_programming/1463.py
+++ python_baekjoon/dynamic_programming/1463.py
@@ -1,3 +1,21 @@
 # 1로 만들기(1463)
 # https://www.acmicpc.net/problem/1463
 
+
+def func(dp, x):
+    dp[2] = 1
+    
+    for n in range(3, x+1):    
+        # 1을 뺴는 경우
+        dp[n] = (dp[n-1] + 1)
+        # 3으로 나누어 떨어지는 경우
+        if n % 3 == 0:        
+            dp[n] = min(dp[n], dp[n // 3] + 1)
+        # 2로 나누어 떨어지는 경우
+        if n % 2 == 0:
+            dp[n] = min(dp[n], dp[n // 2] + 1)
+            
+    return dp[x]
+
+dp = [0] * 1000001
+print(func(dp, int(input())))
