12ff2ab0202f825a415377a8478a1a7cd86962a7
jongwanra <najongsi@gmail.com>
2021-08-17 09:43:19 +0900

    modify problem

--- python_baekjoon/단계별로풀어보기/03-for문/2439.py
+++ python_baekjoon/단계별로풀어보기/03-for문/2439.py
@@ -1,8 +1,7 @@
 # 별 찍기 - 2(2439)
 # https://www.acmicpc.net/problem/2439
 
-n = int(input())
+n = int(input()) 
 
 for i in range(1, n+1):
-    print(" " * (n-i), end="")
-    print("*" * i)
+    print(" " * (n-i) + "*" * i)
