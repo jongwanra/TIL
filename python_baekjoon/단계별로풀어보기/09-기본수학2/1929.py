b0d1686ac59ae8b1fc564c1e90ed0675f2c53a2b
jongwanra <najongsi@gmail.com>
2021-08-26 11:43:22 +0900

    modify problem

--- python_baekjoon/단계별로풀어보기/09-기본수학2/1929.py
+++ python_baekjoon/단계별로풀어보기/09-기본수학2/1929.py
@@ -1,6 +1,8 @@
 # 소수 구하기(1929)
 # https://www.acmicpc.net/problem/1929
 
+# 방법 1
+
 import math
 def check_prime_num(num):
     '''
@@ -22,3 +24,26 @@ m, n = map(int, input().split())
 for num in range(m, n+1):
     if check_prime_num(num) == 1:
         print(num)
+
+
+
+# 방법 2(에라토스테네스의 체)
+
+import math
+def eratosthenes(num):
+    lst = [True] * (num+1)
+    lst[1] = False 
+    
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if lst[i]:
+            j = 2
+            while i * j <= num:
+                lst[i * j] = False
+                j += 1
+    return lst
+
+m, n = map(int, input().split())
+res = eratosthenes(n)
+for i in range(m, n + 1):
+    if res[i]:
+        print(i)
