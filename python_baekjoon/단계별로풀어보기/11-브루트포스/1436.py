df99334198c25adcc65f25f126f308bdbbb52172
jongwanra <najongsi@gmail.com>
2021-09-01 16:16:50 +0900

    add problem of bruth force

---
+++ python_baekjoon/단계별로풀어보기/11-브루트포스/1436.py
@@ -0,0 +1,15 @@
+# 영화감독 숌(1436)
+# https://www.acmicpc.net/problem/1436
+
+n = int(input())
+
+res = []
+count = 0
+data = 1
+while count < n:   
+    if str(data).count('666') >= 1:
+        res.append(data)
+        count += 1
+    data += 1
+
+print(res[-1])
