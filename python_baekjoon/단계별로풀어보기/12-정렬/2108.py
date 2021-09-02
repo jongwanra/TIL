4404064e324221e451276435f27a95d2b9fafe53
jongwanra <najongsi@gmail.com>
2021-09-02 11:32:22 +0900

    add problem 2108

---
+++ python_baekjoon/단계별로풀어보기/12-정렬/2108.py
@@ -0,0 +1,39 @@
+# 통계학(2108)
+# https://www.acmicpc.net/problem/2108
+    
+from sys import stdin
+
+# 최빈값 반환(가장 많이 나타나는 값이 중복일 경우, 두 번째로 작은 값 출력)
+def min_func(dic):
+    sorted_dic = sorted(dic.items(), key= lambda x: x[0])
+    sorted_dic = sorted(sorted_dic, key=lambda x:x[1], reverse=True)
+
+    if sorted_dic[0][1] != sorted_dic[1][1]:
+        return sorted_dic[0][0]
+    return sorted_dic[1][0]
+
+
+n = int(stdin.readline())
+
+sum = 0
+datas = []
+dic = {}
+for _ in range(n):
+    tmp = int(stdin.readline())
+    sum += tmp
+    datas.append(tmp)    
+    try:
+        dic[tmp] += 1
+    except:
+        dic[tmp] = 1
+
+datas.sort()
+print(round(sum/n)) # 산술평균
+print(datas[len(datas)//2]) # 중앙값
+
+# 최빈값
+if len(datas) == 1:
+    print(datas[0])
+else:
+    print(min_func(dic))
+print(abs(datas[0] - datas[len(datas) - 1])) # 범위
