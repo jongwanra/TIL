b938e29b6540f07eb017314ce8755f5aadc698ab
jongwanra <najongsi@gmail.com>
2021-09-03 09:52:03 +0900

    modify file

--- javascript/async/09_callback.js
+++ javascript/async/09_callback.js
@@ -5,17 +5,19 @@
 // - passed as an argument to another function, and, (다른 함수의 인자로서 이용되는 함수)
 // - is invoked after some kind of event (어떤 이벤트에 의해 호출되어지는 함수)
 
-
-// JavaScript is synchronous. 
+// JavaScript is synchronous. (자바스크립트는 동기적으로 순서대로 실행된다.)
 // Execute the code block in order after hoisting.
 // hoisting: var, function declaration 자동적으로 선언들이 제일 위로 올라가는 것
 
 // 비동기: 언제 코드가 실행될지 예측할 수 없는 것
 console.log('1');
-// 브라우저 api는 무조건 브라우저에게 요청을 보내게 됨.
+// 브라우저 api는 무조건 브라우저에게 요청을 보내게 됨. (전달해준 함수를 나중에 불러줘 라는 개념: callback)
 setTimeout(()=> console.log('2'), 1000);
-console.log('3');
+for (let i = 0; i < 1000; i++){
+    console.log(i);
+}
 
+//콜백은 두 가지로 나뉜다.
 // Synchronous callback (즉각, 동기적으로 실행하는)
 function printImmediately(print) {
     print();
@@ -65,19 +67,12 @@ const password = prompt('enter your password');
 userStorage.loginUser(
     id,
     password,
-    user => {
-    userStorage.getRoles(
-        user,
-        userWithRole => {
-            alert(`Hello ${userWithRole.name}, you have a ${userWithRole.role} role`);
+    user => {userStorage.getRoles(user, userWithRole => { 
+        alert(`Hello ${userWithRole.name}, you have a ${userWithRole.role} role`);
     },
-    error => {
-        console.log(error);
-    }
-);    
+    error => console.log(error)
+    );    
 },
- error => {
-     console.log(error);
-    }
+error => console.log(error)
 );
 
