c2e240ace2dba69186f0719c5beb7f2080e8435d
jongwanra <najongsi@gmail.com>
2021-09-05 11:54:48 +0900

    add problems of backtracking

--- python_baekjoon/brute_force/2529.py
+++ python_baekjoon/brute_force/2529.py
@@ -33,3 +33,40 @@
 
 # print(str(res[0]).zfill(k+1))
 # print(str(res[1]).zfill(k+1))
+
+
+def possible(item1, item2, symbol):
+    if symbol == '<':
+        return item1 < item2
+    if symbol == '>':
+        return item1 > item2
+
+
+def backtracking(idx, string):
+    global min_value
+    global visited
+    global max_value
+    if idx == n + 1:
+        if not len(min_value):
+            min_value = string
+        else:
+            max_value = string
+        return
+    for i in range(10):
+        if visited[i]:
+            continue
+        if idx == 0 or possible(string[-1], str(i), stack[idx - 1]):
+            visited[i] = True
+            backtracking(idx + 1, string + str(i))
+            visited[i] = False
+
+
+if __name__ == '__main__':
+    n = int(input())
+    stack = list(input().split())
+    max_value = ''
+    min_value = ''
+    visited = [False] * 10
+    backtracking(0, '')
+    print(max_value)
+    print(min_value)
