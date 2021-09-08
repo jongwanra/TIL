4f2ef14753e06f522343d41cc6002188f42b0133
jongwanra <najongsi@gmail.com>
2021-09-08 11:13:04 +0900

    add probelms of kruskal, topologySort

---
+++ python_이것이_코딩테스트다/graph_theory/ex_10-5_kruskal.py
@@ -0,0 +1,50 @@
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    
+    if a < b:
+        parent[b] = a
+    else:
+        parent[b] = a
+    
+v, e = map(int, input().split())
+parent = [0] * (v + 1)
+
+edges = []
+result = 0
+
+for i in range(1, v + 1):
+    parent[i] = i
+
+for _ in range(e):
+    a, b, cost = map(int, input().split())
+    edges.append((cost, a, b))
+    
+edges.sort()
+
+for edge in edges:
+    cost, a, b = edge
+    
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += cost
+
+print(result)
+
+'''
+7 9
+1 2 29
+1 5 75
+2 3 35
+2 6 34
+3 4 7
+4 6 23
+4 7 13
+5 6 53
+6 7 25
+'''
