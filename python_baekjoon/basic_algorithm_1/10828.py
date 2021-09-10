87b34efba20bc3c5b37b6a4a4a488ce68af35217
jongwanra <najongsi@gmail.com>
2021-09-10 19:55:11 +0900

    add stack problem

---
+++ python_baekjoon/basic_algorithm_1/10828.py
@@ -0,0 +1,29 @@
+# 스택(10828)
+# https://www.acmicpc.net/problem/10828
+from sys import stdin
+
+if __name__ == "__main__":
+    input = stdin.readline    
+    n = int(input())
+    stack = []
+    for _ in range(n):
+        datas = input().split()
+        if datas[0] == "push":
+            stack.append(datas[1])
+        elif datas[0] == "top":
+            if len(stack) == 0:
+                print(-1)
+            else:
+                print(stack[len(stack) - 1])
+        elif datas[0] == "size":
+            print(len(stack))
+        elif datas[0] == "empty":
+            if len(stack) >= 1:
+                print(0)
+            else:
+                print(1)
+        elif datas[0] == "pop":
+            if len(stack) == 0:
+                print(-1)
+            else:
+                print(stack.pop())
