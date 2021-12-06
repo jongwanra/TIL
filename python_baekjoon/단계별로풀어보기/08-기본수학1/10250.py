03d8167fc3a760356f8585aa24c27f58472dd4b6
jongwanra <najongsi@gmail.com>
2021-12-06 13:12:14 +0900

    [211206] 나머지 업로드

--- python_baekjoon/단계별로풀어보기/08-기본수학1/10250.py
+++ python_baekjoon/단계별로풀어보기/08-기본수학1/10250.py
@@ -1,5 +1,23 @@
 # ACM 호텔(10250)
 # https://www.acmicpc.net/problem/10250
+# 18분 소요(21.12.06)
+ 
+n = int(input()) 
+for _ in range(n):
+    # 호텔의 높이, 너비, 몇번째 손님인지 입력
+    h, w, n = map(int, input().split()) 
+    if n % h == 0:
+        dong = h
+        hosu = n // h
+    else:
+        dong = n % h
+        hosu = n // h + 1
+    print(f"{dong}{str(hosu).zfill(2)}")
+
+
+
+# ACM 호텔(10250)
+# https://www.acmicpc.net/problem/10250
 
 n = int(input())
 for _ in range(n):
