a7fe634567c0a28dc878700da9678edc8905721c
jongwanra <najongsi@gmail.com>
2021-12-08 22:55:46 +0900

    [211208] 백준 문제 추가

--- python_baekjoon/basic_algorithm_1/9012.py
+++ python_baekjoon/basic_algorithm_1/9012.py
@@ -1,5 +1,25 @@
 # 괄호(9012)
 # https://www.acmicpc.net/problem/9012
+# 해결 / 10분 소요 / 21.12.08
+n = int(input())
+for _ in range(n):
+    problem = input()
+    cnt = 0
+    for p in problem:
+        if p == '(':
+            cnt += 1
+        else:
+            cnt -= 1
+        if cnt < 0:
+            break
+    print('NO') if cnt != 0 else print('YES')
+        
+    
+
+
+
+# 괄호(9012)
+# https://www.acmicpc.net/problem/9012
 # 품(21.09.10) / 7분 이내
 
 # 괄호의 갯수를 '('이면 1추가, ')' 이면 -1 해서 판단 여부를 반환
--- python_baekjoon/data_structure/10773.py
+++ python_baekjoon/data_structure/10773.py
@@ -1,5 +1,20 @@
 # 제로(10773)
 # https://www.acmicpc.net/problem/10773
+# 해결 / 4분 30초 / 21.12.08
+from sys import stdin
+input = stdin.readline
+
+n = int(input())
+stack = []
+for _ in range(n):
+    tmp = int(input())
+    stack.pop() if tmp == 0 else stack.append(tmp)
+print(sum(stack))
+
+
+
+# 제로(10773)
+# https://www.acmicpc.net/problem/10773
 
 # 스택을 이용한 풀이
 stack = []
