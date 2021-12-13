61392dd0098a3be0f4f1c6f1330b9a6e3c604923
jongwanra <najongsi@gmail.com>
2021-12-13 22:59:42 +0900

    [211213] add DFS problem

--- python_baekjoon/graph_searching/1260.py
+++ python_baekjoon/graph_searching/1260.py
@@ -1,5 +1,36 @@
 # DFS와 BFS(1260)
 # https://www.acmicpc.net/problem/1260 
+from sys import stdin
+def dfs(graph, visited, v):
+    visited[v] = True
+    print(v, end=" ")
+    
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph,visited, i)
+
+# 정점, 간선, 시작 노드 입력
+vertex, edge, start = map(int, stdin.readline().rstrip().split())
+
+# 인접 리스트 방식으로 그래프 초기화
+graph = [[] for _ in range(vertex+1)]
+for _ in range(edge):
+    v1, v2 = map(int, stdin.readline().rstrip().split())
+    graph[v1].append(v2)
+    graph[v2].append(v1)
+print(graph)    
+#  인접리스트 오름차순으로 정렬
+for i in range(vertex + 1):
+    graph[i] = sorted(graph[i])
+
+visited = [False] * (vertex + 1)
+dfs(graph, visited, start)
+
+
+
+
+# DFS와 BFS(1260)
+# https://www.acmicpc.net/problem/1260 
 
 from collections import deque
 
--- python_이것이_코딩테스트다/DFS_BFS/ex_5-8_dfs.py
+++ python_이것이_코딩테스트다/DFS_BFS/ex_5-8_dfs.py
@@ -1,11 +1,12 @@
 # DFS 메서드 정의(인접리스트)
 def dfs_list(graph, v, visited):
+    # [False,True,True,False,False,False,True,True,False]
     # 현재 노드를 방문 처리
     visited[v] = True
     print(v, end=" ")
-
+    # 1 2 7 6 
     # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
-    for i in graph[v]:
+    for i in graph[v]: # [2, 3, 8] / [ 7] / [ 6, 8] /
         if not visited[i]:
             dfs_list(graph, i, visited)
 
