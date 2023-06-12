7e1bb0bfa08ad292ca078b657f584339a9ab34e3
jongwanra <najongsi@gmail.com>
2023-06-12 12:00:49 +0900

    seperate this_is_java

--- this_is_java/src/AboutClass.java
+++
@@ -1,4 +0,0 @@
-public class AboutClass { public static void main(String[] args) {
-        /* Object Oriented Programming */
-    }
-}
--- this_is_java/src/CarExample.java
+++
@@ -1,8 +0,0 @@
-public class CarExample {
-
-    public static void main(String[] args) {
-        Car car = Car.createCar("Germany", "Grander", "red");
-
-        System.out.println("car = " + car);
-    }
-}
