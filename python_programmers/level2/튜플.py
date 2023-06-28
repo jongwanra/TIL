8fb37c389820e34117730cba98c52fb8caf4b60d
jongwanra <najongsi@gmail.com>
2023-06-28 15:20:05 +0900

    add Programmers level2 튜플

--- python_programmers/level2/튜플.py
+++ python_programmers/level2/튜플.py
@@ -8,4 +8,10 @@ def solution(s):
         # 숫자가 처음 나온 경우
         if s[i].isdigit() and int(s[i]) >= 0 and int(s[i]) <= 9 and numStartIdx == -1:
             numStartIdx = i
-        elif (s[i] == '}' oras
+        elif (s[i] == '}' or s[i] == ',') and numStartIdx != -1:
+            counter[s[numStartIdx:i]] += 1
+            numStartIdx = -1
+
+    for key in dict(sorted(counter.items(), key=lambda x:x[1], reverse=True)):
+        answer.append(int(key))
+    return answer
