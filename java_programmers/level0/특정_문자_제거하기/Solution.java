21aa177724bb7ae564b98f67ec73376b99541ab3
jongwanra <najongsi@gmail.com>
2023-07-15 20:56:56 +0900

    add programmers level1 나머지가 1이 되는 수 찾기

--- java_programmers/level0/특정_문자_제거하기/Solution.java
+++ java_programmers/level0/특정_문자_제거하기/Solution.java
@@ -3,7 +3,7 @@ package level0.특정_문자_제거하기;
 public class Solution {
 
     public String solution(String my_string, String letter) {
-        return my_string.replaceAll(letter, "");;
+        return my_string.replaceAll(letter, "");
     }
     public static void main(String[] args) {
         Solution solution = new Solution();
