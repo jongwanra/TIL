9583a75d1c9b06f3ea95cbf110679f89c6a8329c
jongwanra <najongsi@gmail.com>
2023-07-18 13:12:30 +0900

    add god-of-java chapter21

---
+++ god-of-java/src/chapter21/WildcardGeneric.java
@@ -0,0 +1,13 @@
+package chapter21;
+
+public class WildcardGeneric<W> {
+    W wildcard;
+
+    public void setWildcard(W wildcard) {
+        this.wildcard = wildcard;
+    }
+
+    public W getWildcard() {
+        return wildcard;
+    }
+}
