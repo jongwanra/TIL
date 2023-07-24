a721205dd5ac48e576b692e7744a3bfb3037048a
jongwanra <najongsi@gmail.com>
2023-07-24 11:00:09 +0900

    update got-of-java chapter20

--- god-of-java/src/chapter20/JavaLangNumber.java
+++ god-of-java/src/chapter20/JavaLangNumber.java
@@ -1,14 +1,16 @@
 package chapter20;
 
+import java.math.BigInteger;
 import java.util.Arrays;
 
 public class JavaLangNumber {
     public static void main(String[] args) {
         JavaLangNumber sample = new JavaLangNumber();
-        sample.numberTypeCheck();
+//        sample.numberTypeCheck();
+        sample.integerMinMaxCheckBinary();
     }
 
-    private void numberTypeCheck() {
+    protected void numberTypeCheck() {
         String value1 = "3";
         String value2 = "5";
         byte byte1 = Byte.parseByte(value1);
@@ -23,6 +25,14 @@ public class JavaLangNumber {
         Integer refInt2 = Integer.valueOf(value2);
         System.out.println(refInt1 + refInt2 + "7");
 
+    }
+
+    public void integerMinMaxCheckBinary() {
+        System.out.println("Integer BINARY min=" + Integer.toBinaryString(Integer.MIN_VALUE));
+        System.out.println("Integer BINARY max=" + Integer.toBinaryString(Integer.MAX_VALUE));
+
+        System.out.println("Integer HEX min=" + Integer.toHexString(Integer.MIN_VALUE));
+        System.out.println("Integer HEX max=" + Integer.toHexString(Integer.MAX_VALUE));
 
     }
 }
--- god-of-java/src/chapter20/JavaLangSystem.java
+++ god-of-java/src/chapter20/JavaLangSystem.java
@@ -3,11 +3,22 @@ package chapter20;
 public class JavaLangSystem {
     public static void main(String[] args) {
         JavaLangSystem sample = new JavaLangSystem();
-        sample.systemPropertiesCheck();
+        sample.numberMinMaxElapsedCheck();
     }
 
-    private void systemPropertiesCheck() {
+    protected void systemPropertiesCheck() {
         System.out.println("java.version=" + System.getProperty("java.version"));
         System.out.println("JAVA_HOME="+System.getenv("JAVA_HOME"));
+    }
+
+    private void numberMinMaxElapsedCheck() {
+        JavaLangNumber numberSample = new JavaLangNumber();
+        long startTime = System.currentTimeMillis();
+        long startNanoTime = System.nanoTime();
+
+        numberSample.numberTypeCheck();
+
+        System.out.println("Milli second=" + (System.currentTimeMillis() - startTime));
+        System.out.println("Nano second=" + (System.nanoTime() - startNanoTime) / 1_000_000);
     }
 }
