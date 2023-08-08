d78c4cd08e180a52f54e1d6fdbb2ae5f974c6224
jongwanra <najongsi@gmail.com>
2023-08-08 12:40:42 +0900

    add algorithm 재귀함수 출력

---
+++ java_algorithm/inflearn_algorithm_lecture/src/dfs_and_bfs/재귀함수/Main.java
@@ -0,0 +1,18 @@
+package dfs_and_bfs.재귀함수;
+
+public class Main {
+
+    public void dfs(int n) {
+        if(n == 0) {
+            return;
+        }
+        dfs(n - 1);
+        System.out.print(n + " ");
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.dfs(3);
+    }
+
+}
