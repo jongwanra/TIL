6560038954231d1241c4f493e8cbcaf1fe611e82
jongwanra <najongsi@gmail.com>
2023-08-17 10:55:35 +0900

    add algorithm 조합수(메모이제이션)

---
+++ java_algorithm/inflearn_algorithm_lecture/src/dfs_and_bfs/조합수_메모이제이션/Main.java
@@ -0,0 +1,47 @@
+package dfs_and_bfs.조합수_메모이제이션;
+
+import java.util.*;
+
+public class Main {
+	static int[][] dp;
+	
+	public static void main(String[] args) {
+		Main main = new Main();
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int r = sc.nextInt();
+		dp = new int[n + 1][n + 1];
+		System.out.println(main.lectureDfs(n, r));
+	}
+	
+	private int lectureDfs(int n, int r) {
+		// System.out.println("n : " + n + " r: " + r);
+		
+		if (n == r || r == 0) {
+			return 1;
+		}
+		
+		if (dp[n][r] > 0) {
+			return dp[n][r];
+		}
+		
+		return dp[n][r] = lectureDfs(n - 1, r - 1) + lectureDfs(n - 1, r);
+	}
+	
+	private int myDfs(final int n, final int r) {
+		
+		if (n == -1) {
+			return 0;
+		}
+		if (r == 0) {
+			return 1;
+		}
+		
+		if (dp[n][r] > 0) {
+			return dp[n][r];
+		}
+		
+		dp[n][r] = myDfs(n - 1, r - 1) + myDfs(n - 1, r);
+		return dp[n][r];
+	}
+}
