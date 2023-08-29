e70a0a02c7b011a2577772c1e8f5edcda80f9e93
jongwanra <najongsi@gmail.com>
2023-08-30 01:17:57 +0900

    update error_and_exception.md

--- java_study/src/error_and_exception/ExceptionTest.java
+++ java_study/src/error_and_exception/ExceptionTest.java
@@ -3,7 +3,46 @@ package error_and_exception;
 public class ExceptionTest {
 	public static void main(String[] args) {
 		ExceptionTest main = new ExceptionTest();
-		main.testException();
+		// main.testException();
+		
+		// main.checkRunTimeException();
+		main.checkPerformanceWithTryCatch(100_000_000);
+		main.checkPerformanceWithoutTryCatch(100_000_000);
+		
+	}
+	
+	private void checkPerformanceWithoutTryCatch(final int size) {
+		int[] arr = new int[size];
+		long start = System.nanoTime();
+		for (int i = 0; i < size; i++) {
+			arr[i] = i;
+		}
+		long end = System.nanoTime();
+		
+		System.out.println("없을 때: " + (end - start) / 1_000_000 + "ms");
+	}
+	
+	private void checkPerformanceWithTryCatch(final int size) {
+		int[] arr = new int[size];
+		long start = System.nanoTime();
+		
+		for (int i = 0; i < size; i++) {
+			try {
+				arr[i] = i;
+			} catch (Exception e) {
+				System.out.println();
+			}
+		}
+		
+		long end = System.nanoTime();
+		
+		System.out.println("Try-Catch문 있을 떄: " + (end - start) / 1_000_000 + "ms");
+	}
+	
+	private void checkRunTimeException() {
+		int[] arr = new int[10];
+		arr[11] = 0;
+		
 	}
 	
 	private void testException() {
