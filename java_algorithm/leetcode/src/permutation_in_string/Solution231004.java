e11be5357d89a28b19be3f259bedd50cd11730dc
jongwanra <najongsi@gmail.com>
2023-10-04 11:46:44 +0900

    update leetcode permutation_in_string

---
+++ java_algorithm/leetcode/src/permutation_in_string/Solution231004.java
@@ -0,0 +1,48 @@
+package permutation_in_string;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution231004 {
+	public boolean checkInclusion(String s1, String s2) {
+		int[] originCount = new int[26];
+		int[] copiedCount = new int[26];
+
+		for (int i = 0; i < s1.length(); i++) {
+			int idx = s1.charAt(i) - 'a';
+			originCount[idx]++;
+			copiedCount[idx]++;
+		}
+
+		int left = 0;
+		int right = 0;
+
+		while (right < s2.length()) {
+			// 유효한 문자일 경우
+			if (copiedCount[s2.charAt(right) - 'a'] > 0) {
+				copiedCount[s2.charAt(right) - 'a']--;
+				if ((right - left + 1) == s1.length()) {
+					return true;
+				}
+				right++;
+				continue;
+			}
+
+			if (originCount[s2.charAt(left) - 'a'] > 0) {
+				copiedCount[s2.charAt(left) - 'a']++;
+				left++;
+				continue;
+			}
+
+			right++;
+			left = right;
+
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+		Solution231004 main = new Solution231004();
+		System.out.println(main.checkInclusion("ba", "eidbaooo"));
+	}
+}
